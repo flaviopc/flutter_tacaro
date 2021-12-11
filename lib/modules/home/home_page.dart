ccf6a6e60147d7160d38ffef5ce2877895a66dbd
Flávio Paixão <flavio_pc@outlook.com>
2021-12-11 14:25:58 -0300

    criação card chart

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:ta_caro/shared/models/user_model.dart';
 import 'package:ta_caro/shared/themes/app_theme.dart';
 import 'package:ta_caro/shared/widgets/bottom_navigator/app_bottom_navigator.dart';
-import 'package:ta_caro/shared/widgets/card_product/card_product.dart';
+import 'package:ta_caro/shared/widgets/cards/card_chart/card_chart.dart';
+import 'package:ta_caro/shared/widgets/cards/card_product/card_product.dart';
 import 'package:ta_caro/shared/widgets/list_tile/app_list_tile.dart';
 
 class HomePage extends StatefulWidget {
@@ -27,37 +28,84 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppTheme.colors.background,
-      body: Stack(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 126,
-                width: MediaQuery.of(context).size.width,
-                child: ListView.builder(
-                  itemCount: 2,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) => CardProduct(
-                    like: index % 2 == 0,
-                  ),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          children: [
+            SafeArea(
+              top: true,
+              bottom: false,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CardChart(
+                            value: 432.90,
+                            percent: 0.8,
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text("Preço dos produtos").label,
+                          SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 126,
+                      width: MediaQuery.of(context).size.width,
+                      child: ListView.builder(
+                        itemCount: 2,
+                        scrollDirection: Axis.horizontal,
+                        itemBuilder: (context, index) => CardProduct(
+                          like: index % 2 == 0,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text("Últimas compras").label,
+                          SizedBox(
+                            height: 10,
+                          ),
+                          AppListTile(),
+                          AppListTile(),
+                          AppListTile(),
+                          SizedBox(
+                            height: 80,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
               ),
-              AppListTile(),
-              AppListTile(),
-              AppListTile(),
-            ],
-          ),
-          Positioned(
-            bottom: 10,
-            right: 20,
-            left: 20,
-            child: AppBottomNavigator(
-              currentIndex: currentIndex,
-              onChanged: changeIndex,
             ),
-          )
-        ],
+            Positioned(
+              bottom: 10,
+              right: 20,
+              left: 20,
+              child: AppBottomNavigator(
+                currentIndex: currentIndex,
+                onChanged: changeIndex,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
